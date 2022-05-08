c8d45bc280f98f8175563d0295ec09fa82d65fac
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-05-08 17:42:50 -0400

    employee test done

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,14 @@
-const Employee = require('../LIB/Employee')
+const Employee = require('../LIB/Employee');
+
+describe("Employee", () => {
+    describe("Initialization", () => {
+        it("should instantiate an employee instance", () => {
+            //arrange
+            const employee = new Employee();
+            //act
+
+            //assert
+            expect(typeof(employee)).toBe("object");
+        });
+    });
+});
